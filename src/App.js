a5f2b4c31dd2bdd5aa41cd595db72bcb7bcea537
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2021-11-27 14:02:29 -0300

    Add the score board to game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ScoreBoard from "./components/ScoreBoard";
 import blueCandy from './images/blue-candy.png';
 import greenCandy from './images/green-candy.png';
 import orangeCandy from './images/orange-candy.png';
@@ -22,13 +23,16 @@ const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
   const [squareBeingDragged, setSquareBeingDragged] = useState(null);
   const [squareBeingReplaced, setSquareBeingReplaced] = useState(null);
+  const [scoreDisplay, setScoreDisplay] = useState(0);
 
   const checkForColumnOfThree = () => {
     for (let i = 0; i <= 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
       const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
 
-      if (columnOfThree.every( square => currentColorArrangement[square] === decidedColor)) {
+      if (columnOfThree.every( square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 3);
         columnOfThree.forEach( square => currentColorArrangement[square] = blank); //cleaning the image with blank
         return true;
       }
@@ -39,8 +43,10 @@ const App = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
 
-      if (columnOfFour.every( square => currentColorArrangement[square] === decidedColor)) {
+      if (columnOfFour.every( square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 4);
         columnOfFour.forEach( square => currentColorArrangement[square] = blank); //cleaning the image with blank
         return true;
       }
@@ -52,10 +58,12 @@ const App = () => {
       const rowOfThree = [i, i + 1, i + 2];
       const decidedColor = currentColorArrangement[i];
       const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (notValid.includes(i)) continue
 
-      if (rowOfThree.every( square => currentColorArrangement[square] === decidedColor)) {
+      if (rowOfThree.every( square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 3);
         rowOfThree.forEach( square => currentColorArrangement[square] = blank); //cleaning the image with blank
         return true;
       }
@@ -67,10 +75,12 @@ const App = () => {
       const rowOfFour = [i, i + 1, i + 2, i + 3];
       const decidedColor = currentColorArrangement[i];
       const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 38, 39, 46, 47, 53, 54, 55, 62, 63, 64];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (notValid.includes(i)) continue
 
-      if (rowOfFour.every( square => currentColorArrangement[square] === decidedColor)) {
+      if (rowOfFour.every( square => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 4);
         rowOfFour.forEach( square => currentColorArrangement[square] = blank); //cleaning the image with blank
         return true;
       }
@@ -95,17 +105,14 @@ const App = () => {
   }
 
   const dragStart = (e) => {
-    console.log('drag start')
     setSquareBeingDragged(e.target);
   }
 
   const dragDrop = (e) => {
-    console.log('drag dragDrop')
     setSquareBeingReplaced(e.target);
   }
 
   const dragEnd = (e) => {
-    console.log('drag dragEnd')
     const squareBeingDraggedId = parseInt(squareBeingDragged.getAttribute('data-id'));
     const squareBeingReplacedId = parseInt(squareBeingReplaced.getAttribute('data-id'));
     
@@ -153,6 +160,8 @@ const App = () => {
 
   }
 
+  console.log(scoreDisplay)
+
 
   useEffect(() => {
     createBoard();
@@ -173,11 +182,11 @@ const App = () => {
   
   return (
     <div className="app">
+      <ScoreBoard score={scoreDisplay}/>
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
           <img 
             key={index}
-            // style={{backgroundColor : candyColors}}
             src={candyColor}
             alt={candyColor}
             data-id={index}
---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,9 @@
+const ScoreBoard = ({score}) => { 
+  return (
+    <div className="score-board">
+      <h2>Your score is {score}</h2>
+    </div>
+  )
+}
+
+export default ScoreBoard;
